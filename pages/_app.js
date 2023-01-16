85601ec31253ec67b37560d0f4a020caa888f213
Gururajj77 <gururajtakeshi@gmail.com>
2023-01-16 17:37:53 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.scss";
+import "tailwindcss/tailwind.css";
+
+import react, { useEffect, useState } from "react";
+import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
